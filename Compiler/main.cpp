aa6f00182255589bf7272ff0901c526a5d66ea13
Zeeshan Afzal <zeeshanafzal@184.9.168.191.isp.timbrasil.com.br>
2015-04-20 13:05:42 +0500

    Initial Commit

---
+++ Compiler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Compiler
+//
+//  Created by Zeeshan Afzal on 20/04/2015.
+//  Copyright (c) 2015 EagleSoft. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
